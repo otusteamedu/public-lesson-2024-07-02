d058b041697061ce7ec4728933b33bd97d64f5c1
Mikhail Kamorin <m.v.kamorin@gmail.com>
2024-07-01 23:16:09 +0300

    Use Symfony workflow to define business logic

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -5,27 +5,23 @@ namespace App\Service;
 use App\Entity\Order;
 use App\Entity\OrderStateEnum;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class OrderService
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly WorkflowInterface $orderProcessStateMachine,
     ) {
     }
 
     public function changeState(Order $order, OrderStateEnum $newState): bool
     {
-        $currentState = OrderStateEnum::from($order->getState());
-        if (
-                ($currentState === OrderStateEnum::New && $newState === OrderStateEnum::Accepted) ||
-                ($currentState === OrderStateEnum::Accepted && $newState === OrderStateEnum::Paid) ||
-                ($currentState === OrderStateEnum::Paid && $newState === OrderStateEnum::Sent) ||
-                ($currentState === OrderStateEnum::Sent && $newState === OrderStateEnum::Finished)
-            ) {
-                $order->setState($newState->value);
-                $this->entityManager->flush();
+        if ($this->orderProcessStateMachine->can($order, $newState->value)) {
+            $order->setState($newState->value);
+            $this->entityManager->flush();
 
-                return true;
+            return true;
         }
 
         return false;
